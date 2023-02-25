393a7c77f2e498fac2ec066a8558a868be374ae8
Kevin You <kevin.you@outlook.com>
2023-02-25 02:01:34 -0600

    Potentially break everything or potentially deploy site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://kyouko.me',
+	site: '/blog',
 	integrations: [mdx(), sitemap()],
 });
