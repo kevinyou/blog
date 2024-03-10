10515db14ae44212ef91415d47c2471887c84e94
Kevin You <kevin.you@outlook.com>
2024-03-10 13:16:50 -0500

    Add react to configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kyouko.me',
 	base: '/blog',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), react()],
 });
